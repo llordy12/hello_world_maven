40934b7ab58a674d173ee6b26441da10376b295a
Hector Silva Santos <promotoraeliana@yahoo.com.br>
2024-11-26 12:31:26 -0300

    maven2

--- src/main/java/com/generation/helloworld/controller/HelloWorldController.java
+++ src/main/java/com/generation/helloworld/controller/HelloWorldController.java
@@ -1,5 +1,8 @@
 package com.generation.helloworld.controller;
 
+
+import java.util.List;
+
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -7,9 +10,27 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 @RequestMapping("/hello-world")
 public class HelloWorldController {
+	
+	
 	@GetMapping
 	public String helloWorld() {
 		return "Hello World!!!";
 	}
+	
+	 @GetMapping("/bsm")
+	    public List<String> listBsm() {
+	        return List.of(
+	                "Proatividade",
+	                "Orientação ao Futuro",
+	                "Trabalho em Equipe",
+	                "Responsabilidade Pessoal",
+	                "Comunicação",
+	                "Mentalidade de Crescimento",
+	                "Persistencia",
+	                "Atenção aos Detalhes"
+	        );
+	    }
+	 
+
  
 }
